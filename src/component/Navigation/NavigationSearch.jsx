e4846604bdd5dc3278421fb14a4c26a01451a95b
hyebinjo <hyebinjo91@gmail.com>
2022-07-13 11:05:16 +0900

    feat: 이미지 없을시 imageReady true로 처리

--- src/component/Navigation/NavigationSearch.jsx
+++ src/component/Navigation/NavigationSearch.jsx
@@ -1,11 +1,11 @@
-import React,{useState,useEffect,useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
-import { ReactComponent as SearchIco } from "../images/icons/search-svgrepo-com.svg";
+import { ReactComponent as SearchIco } from "../../images/icons/search-svgrepo-com.svg";
 import { useNavigate } from "react-router";
-import { useMovieModel } from "../models/useMovieModel";
+import { useMovieModel } from "../../models/useMovieModel";
 
 export default function NavigationSearch() {
-  const {movies , getMovies} = useMovieModel();
+  const { movies, getMovies } = useMovieModel();
   const searchRef = useRef();
   const searchBoxRef = useRef();
   const [relatedSearch, setRelatedSearch] = useState();
@@ -14,9 +14,9 @@ export default function NavigationSearch() {
   const [recentSearches, setRecentSearches] = useState([]);
   const navigation = useNavigate();
 
-  useEffect(()=>{
+  useEffect(() => {
     getMovies();
-  },[])
+  }, []);
   useEffect(() => {
     function handleClickOutside(event) {
       if (searchBoxRef.current && !searchBoxRef.current.contains(event.target)) {
@@ -41,7 +41,6 @@ export default function NavigationSearch() {
     navigation(`/search/${searchClick}`);
   };
 
-
   const getSearchMovieTitle = (searchInput) => {
     const result = movies?.results.filter((movie) => {
       return movie.original_title.toLowerCase().slice(0, searchInput.length).includes(searchInput.toLowerCase());
@@ -49,7 +48,6 @@ export default function NavigationSearch() {
     setRelatedSearch(result);
   };
 
-  
   const getRecentlySearch = () => {
     const getRecent = localStorage.getItem("searchRecent");
     setRecentSearches(getRecent !== null && getRecent.split(","));
@@ -62,7 +60,7 @@ export default function NavigationSearch() {
     localStorage.setItem("searchRecent", recentArr);
     setRecentSearches(getRecent !== null && getRecent.split(","));
   };
-  
+
   const searchOnChange = (e) => {
     const searchInput = searchRef.current.value;
     if (searchInput === "") {
@@ -70,46 +68,43 @@ export default function NavigationSearch() {
       return;
     }
     setSearchReady(true);
-    processChanges(searchInput)
+    processChanges(searchInput);
   };
-  const debounce = (callback,delay) => {
+  const debounce = (callback, delay) => {
     let timer;
     return (...args) => {
       clearTimeout(timer);
-      timer = setTimeout(()=> 
-        callback(...args)
-      ,delay)
-    }
-  }
-  const processChanges = debounce((value) => getSearchMovieTitle(value),270);
-  return(
-    
-      <SearchWrap show={searchShow} ref={searchBoxRef}>
-          <form onSubmit={(event) => moveToSearchPath(event)}>
-            <SearchIco />
-            <input ref={searchRef} placeholder="보고싶은 영화 ?" onChange={searchOnChange} onFocus={() => setSearchShow(true)} />
-          </form>
-          <SearchBox show={searchShow}>
-            <RecentWrap show={searchShow}>
-              <SearchOption show={searchShow}>{searchReady ? "추천 검색어" : "최근 검색어"}</SearchOption>
-              {searchReady
-                ? relatedSearch?.map((item, index) => (
-                    <SearchItem show={searchShow} onClick={(event) => moveToSearchBoxPath(event)} key={index}>
-                      {item.original_title}
-                    </SearchItem>
-                  ))
-                : recentSearches.length > 0 &&
-                  recentSearches?.map((item, index) =>
-                    index > 4 ? null : (
-                      <SearchItem onClick={(event) => moveToSearchBoxPath(event)} show={searchShow} key={index}>
-                        {item}
-                      </SearchItem>
-                    )
-                  )}
-            </RecentWrap>
-          </SearchBox>
-        </SearchWrap>
-  )
+      timer = setTimeout(() => callback(...args), delay);
+    };
+  };
+  const processChanges = debounce((value) => getSearchMovieTitle(value), 270);
+  return (
+    <SearchWrap show={searchShow} ref={searchBoxRef}>
+      <form onSubmit={(event) => moveToSearchPath(event)}>
+        <SearchIco />
+        <input ref={searchRef} placeholder="보고싶은 영화 ?" onChange={searchOnChange} onFocus={() => setSearchShow(true)} />
+      </form>
+      <SearchBox show={searchShow}>
+        <RecentWrap show={searchShow}>
+          <SearchOption show={searchShow}>{searchReady ? "추천 검색어" : "최근 검색어"}</SearchOption>
+          {searchReady
+            ? relatedSearch?.map((item, index) => (
+                <SearchItem show={searchShow} onClick={(event) => moveToSearchBoxPath(event)} key={index}>
+                  {item.original_title}
+                </SearchItem>
+              ))
+            : recentSearches.length > 0 &&
+              recentSearches?.map((item, index) =>
+                index > 4 ? null : (
+                  <SearchItem onClick={(event) => moveToSearchBoxPath(event)} show={searchShow} key={index}>
+                    {item}
+                  </SearchItem>
+                )
+              )}
+        </RecentWrap>
+      </SearchBox>
+    </SearchWrap>
+  );
 }
 const SearchWrap = styled.div`
   position: relative;
